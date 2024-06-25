138c018964a4bcfe4a64fbfe4fadd9505ea936ff
Ionita Alexandru <alexvladut03@yahoo.com>
2024-06-25 18:09:45 +0300

    Responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,40 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import { FaHome, FaProductHunt } from "react-icons/fa";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { MdReviews } from "react-icons/md";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 export default function NavBar() {
   return (
-    <div className="flex justify-around items-center bg-amber-500 ">
+    <div className="flex justify-between items-center bg-amber-500 p-4">
       <Image
         src="/Logo.jpeg"
         width={70}
         height={70}
         alt="Logo"
-        className=" rounded-3xl "
+        className="rounded-3xl"
       />
-      <div className="flex gap-8 font-semibold">
+      <div className="hidden lg:flex gap-8 font-semibold">
         <div>Acasa</div>
         <div>Produse</div>
         <div>Despre Noi</div>
         <div>Recenzii</div>
       </div>
-      <button className="lg:p-2 text-white bg-green-600 rounded-xl shadow-md hover:shadow-green-900 hover:text-green-900 ">
+      <div className="lg:hidden flex justify-center gap-5">
+        <FaHome className="text-2xl text-gray-700" />
+        <FaProductHunt className="text-2xl text-gray-700" />
+        <BsFillInfoCircleFill className="text-2xl text-gray-700" />
+        <MdReviews className="text-2xl text-gray-700" />
+      </div>
+      <button className="hidden lg:block p-2 text-white bg-green-600 rounded-xl shadow-md hover:shadow-green-900 hover:text-green-900">
         WhatsApp
       </button>
+      <div className="lg:hidden">
+        <button className="p-2 text-white bg-green-600 rounded-xl shadow-md hover:shadow-green-900 hover:text-green-900">
+          <IoLogoWhatsapp className="text-2xl" />
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Produse.jsx
+++ src/components/Produse.jsx
@@ -7,7 +7,7 @@ export default function Produse() {
       <div className="text-3xl text-center pb-8 pt-8 text-white text-shadow">
         Produse
       </div>
-      <div className="grid grid-cols-3 justify-items-center pl-40 pr-40">
+      <div className="grid lg:grid-cols-3 sm:grid-cols-1 justify-items-center pl-40 pr-40">
         <ProduseCard
           pImageSrc={"/CapaceNegreMiciAudi.jpeg"}
           pName={"Capace Audi Negre 60mm"}
@@ -24,7 +24,7 @@ export default function Produse() {
           pPrice={"100de lei"}
         />
       </div>
-      <div className="grid grid-cols-2 justify-items-center mt-12 pl-80 pr-80">
+      <div className="grid lg:grid-cols-2 sm:grid-cols-1 justify-items-center mt-12 pl-80 pr-80">
         <ProduseCard
           pImageSrc={"/SemnalizariDinamice.jpeg"}
           pName={"Semnalizari Dinamice Audi B8"}
