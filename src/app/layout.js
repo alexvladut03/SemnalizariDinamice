98d2f8a87f3315c06b1270a615236ef7d29b1abf
Ionita Alexandru <alexvladut03@yahoo.com>
2024-06-27 15:27:43 +0300

    Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Produse from "@/components/Produse";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <NavBar />
         <Produse />
+        <Footer />
         {children}
       </body>
     </html>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <div className="grid justify-items-stretch bg-gray-700  text-white font-light p-20 text-xs lg:text-lg">
+      <hr></hr>
+
+      <div className="grid grid-cols-2 pt-3 ">
+        <Image
+          src={"/Logo.jpeg"}
+          width={40}
+          height={40}
+          alt="Loading..."
+          className="rounded-full"
+        ></Image>
+        <div className="flex justify-end text-3xl gap-3">
+          <FaFacebook />
+          <FaInstagram />
+          <FaTiktok />
+        </div>
+      </div>
+
+      <div className="mb-3">
+        <p className="pt-5">Adresa:</p>
+        <p>Dambovita, Crevedia, Str.Berzelor, Nr.111</p>
+        <p className="pt-5">Contact:</p>
+        <p>alexvladut03@gmail.com</p>
+        <p>bratualin0.2003@gmail.com</p>
+        <p>mocanuandrei@gmail.com</p>
+      </div>
+
+      <hr></hr>
+
+      <div className="grid grid-cols-2 pt-3">
+        <div>
+          <p>2024 Relume. Toate drepturile rezervate</p>
+        </div>
+        <div className="lg:flex lg:justify-end lg:gap-5">
+          <p className="lg:hover:underline">Politica de Confidentialitate</p>
+          <p className="lg:hover:underline">Termeni si Conditii</p>
+          <p className="lg:hover:underline">Setari Cookie-uri</p>
+        </div>
+      </div>
+    </div>
+  );
+}
