26bf7cf4763d5d6192af662a72ef0ed2df613a5a
Ionita Alexandru <alexvladut03@yahoo.com>
2024-07-13 22:43:36 +0300

    refactor: Optimize image loading in product page layout and components

--- src/components/admin/forms/productForm.js
+++ src/components/admin/forms/productForm.js
@@ -20,146 +20,161 @@ const ProductForm = ({ formData, action }) => {
   });
 
   return (
-    <Form {...form}>
-      <form action={action} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="id"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>ID</FormLabel>
-              <FormControl>
-                <Input placeholder="ID" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="category"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Category</FormLabel>
-              <FormControl>
-                <Input placeholder="Category" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Product Name" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="price"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Price</FormLabel>
-              <FormControl>
-                <Input placeholder="Price" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="stock"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Stock</FormLabel>
-              <FormControl>
-                <Input placeholder="Stock" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="mainImage"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Main Image</FormLabel>
-              <FormControl>
-                <Input placeholder="Main Image URL" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="gallery"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Gallery</FormLabel>
-              <FormControl>
-                <Input placeholder="Gallery Images URLs" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Input placeholder="Description" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="fitment"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Fitment</FormLabel>
-              <FormControl>
-                <Input placeholder="Fitment" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="characteristics"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Characteristics</FormLabel>
-              <FormControl>
-                <Input placeholder="Characteristics" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button
-          className="bg-black hover:bg-blue-500 text-white rounded-[25px] text-center w-full py-2"
-          type="submit"
-        >
-          Submit
-        </Button>
-      </form>
-    </Form>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-bold my-8">Creează produs</h1>
+      <div className="max-w-2xl p-8 bg-white rounded-lg shadow-md mb-8">
+        <Form {...form}>
+          <form action={action} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Product Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="category"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Category" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="grid grid-cols-3 gap-4">
+              <FormField
+                control={form.control}
+                name="id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>ID</FormLabel>
+                    <FormControl>
+                      <Input placeholder="ID" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="price"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Price</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Price" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="stock"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Stock</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Stock" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormField
+              control={form.control}
+              name="mainImage"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Main Image</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Main Image URL" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="gallery"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Gallery</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Gallery Images URLs" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Description" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="fitment"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Fitment</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Fitment" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="characteristics"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Characteristics</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Characteristics" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              className="bg-black hover:bg-blue-500 text-white rounded-2xl text-center w-full py-2"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
   );
 };
 
