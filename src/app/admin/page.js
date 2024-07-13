fbfbe01cb2ffaf47eab9b0d6032c365329fdc304
mocanu <mocanuandrei61@gmail.com>
2024-07-13 16:58:02 +0300

    refactor: Update ProductPrice component to use lazy loading for images

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const AdminDashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Link href="/admin/produse">Produse</Link>
+    </div>
+  );
 };
 
 export default AdminDashboard;
--- src/app/admin/produse/page.js
+++ src/app/admin/produse/page.js
@@ -9,30 +9,37 @@ import {
 } from "@/components/ui/table";
 import { getProducts } from "../../../../actions/product";
 
+import { MdDeleteForever } from "react-icons/md";
+import { IoDuplicate } from "react-icons/io5";
+import DeleteProductForm from "@/components/admin/products/deleteProductForm";
+import Link from "next/link";
+
 const produse = async () => {
   const products = await getProducts();
 
   return (
-    <Table className="max-w-7xl mx-auto">
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>ID</TableHead>
-          <TableHead>Categorie</TableHead>
-          <TableHead>Nume</TableHead>
-          <TableHead>Pret</TableHead>
-          <TableHead>Stoc</TableHead>
-          <TableHead>Imagine</TableHead>
-          <TableHead>Galerie</TableHead>
-          <TableHead>Descriere</TableHead>
-          <TableHead>Fitment</TableHead>
-          <TableHead>Caracteristici</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          {products.map((product) => (
-            <>
+    <div className="max-w-7xl mx-auto h-screen">
+      <Link href="/admin/produse/nou">Adauga produs</Link>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Categorie</TableHead>
+            <TableHead>Nume</TableHead>
+            <TableHead>Pret</TableHead>
+            <TableHead>Stoc</TableHead>
+            <TableHead>Imagine</TableHead>
+            <TableHead>Galerie</TableHead>
+            <TableHead>Descriere</TableHead>
+            <TableHead>Fitment</TableHead>
+            <TableHead>Caracteristici</TableHead>
+            <TableHead>Actiuni</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.map((product, index) => (
+            <TableRow key={index}>
               <TableCell>{product.id}</TableCell>
               <TableCell>{product.category}</TableCell>
               <TableCell>{product.name}</TableCell>
@@ -43,11 +50,24 @@ const produse = async () => {
               <TableCell>{product.description}</TableCell>
               <TableCell>{product.fitment}</TableCell>
               <TableCell>{product.characteristics}</TableCell>
-            </>
+              <TableCell className="flex text-2xl gap-3">
+                <DeleteProductForm id={product.id} />
+                <form>
+                  <button type="submit">
+                    <IoDuplicate />
+                  </button>
+                </form>
+                <form>
+                  <button type="submit">
+                    <MdDeleteForever />
+                  </button>
+                </form>
+              </TableCell>
+            </TableRow>
           ))}
-        </TableRow>
-      </TableBody>
-    </Table>
+        </TableBody>
+      </Table>
+    </div>
   );
 };
 
