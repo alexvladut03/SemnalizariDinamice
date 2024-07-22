4f3e5d6b6ed1da438571bbf008026707277581de
mocanu <mocanuandrei61@gmail.com>
2024-07-23 00:28:44 +0300

    refactor: Update NavBar component import in layout.js3

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,19 @@
+import DesktopNavBar from "./DesktopNavBar";
+import MobileNavBar from "./MobileNavBar";
+
+const NavBar = () => {
+  return (
+    <header className="bg-black p-4 sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto">
+        <div className="hidden lg:block">
+          <DesktopNavBar />
+        </div>
+        <div className="lg:hidden">
+          <MobileNavBar />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
