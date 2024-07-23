7f9b0fcc5d2acc69ddf7cbaf09a45165bf541bf0
mocanu <mocanuandrei61@gmail.com>
2024-07-23 19:47:35 +0300

    refactor: Remove unused login page component

---
+++ src/middleware.js
@@ -0,0 +1,17 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+
+  if (!token) {
+    // Redirect to login page if the user is not authenticated
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
