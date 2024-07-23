baf64206a4220c99d635910d3df905fd634a73a5
mocanu <mocanuandrei61@gmail.com>
2024-07-24 00:04:37 +0300

    refactor: Update middleware.js to use auth middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,30 +1,11 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+import { auth } from "../auth";
 
-export async function middleware(req) {
-  if (
-    !req.cookies.has("next-auth.session-token") &&
-    req.cookies.has("__Secure-next-auth.session-token")
-  ) {
-    console.log("Relaying auth cookie...");
-    req.cookies.set({
-      ...req.cookies.get("__Secure-next-auth.session-token"),
-      name: "next-auth.session-token",
-    });
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
+    const newUrl = new URL("/login", req.nextUrl.origin);
+    return Response.redirect(newUrl);
   }
-
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-
-  console.log(token);
-  console.log(req);
-
-  if (!token) {
-    // Redirect to login page if the user is not authenticated
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  return NextResponse.next();
-}
+});
 
 export const config = {
   matcher: ["/admin/:path*"],
