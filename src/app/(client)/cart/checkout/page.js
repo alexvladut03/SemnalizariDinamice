74ff82621c04f597c3eb30e1478a423115b093c5
Ionita Alexandru <alexvladut03@yahoo.com>
2024-07-29 14:55:36 +0300

    refactor: Add NavBarCart component to checkout and summary pages

--- src/app/(client)/cart/checkout/page.js
+++ src/app/(client)/cart/checkout/page.js
@@ -1,3 +1,4 @@
+import NavBarCart from "@/components/cart/NavBarCart";
 import CommandBillingData from "@/components/checkout/CommandBillingData";
 import CommandDetails from "@/components/checkout/CommandDetails";
 import CommandPaymentMethod from "@/components/checkout/CommandPaymentMethod";
@@ -8,6 +9,7 @@ import React from "react";
 const Checkout = () => {
   return (
     <div className="bg-gray-100">
+      <NavBarCart activeStep={2} />
       <div className="max-w-4xl mx-auto p-6 min-h-screen">
         <div className="text-2xl font-semibold pb-4">Detalii comanda</div>
         <CommandDetails />
