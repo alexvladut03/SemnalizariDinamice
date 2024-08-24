7d11c4d2596e02bcd6101b1b091a08b3eb1c4126
Ionita Alexandru <alexvladut03@yahoo.com>
2024-08-24 20:25:40 +0300

    refactor: Improve touch events and transition timing in MobileNavBar

--- src/components/navbarClient/MobileNavBar.js
+++ src/components/navbarClient/MobileNavBar.js
@@ -2,45 +2,43 @@
 import { useState, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { FaCartPlus } from "react-icons/fa6";
-import { useCart } from "@/app/context/CartProvider";
-import CartProducts from "./cart/CartProducts";
+import { IoIosMenu } from "react-icons/io";
+import { MdClose } from "react-icons/md";
 
 export default function MobileNavBar() {
-  const { countCartItems } = useCart();
-  const [isCartOpen, setIsCartOpen] = useState(false); // Stare pentru coșul de cumpărături
-  const cartRef = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Stare pentru meniul hamburger
+  const menuRef = useRef(null);
   const startX = useRef(0);
   const startY = useRef(0);
   const currentX = useRef(0);
   const currentY = useRef(0);
 
-  const toggleCart = () => {
-    if (isCartOpen) {
-      closeCart();
+  const toggleMenu = () => {
+    if (isMenuOpen) {
+      closeMenu();
     } else {
-      openCart();
+      openMenu();
     }
   };
 
-  const openCart = () => {
-    setIsCartOpen(true);
-    cartRef.current.style.transition = "transform 0.5s ease";
-    cartRef.current.style.transform = "translateX(0)";
+  const openMenu = () => {
+    setIsMenuOpen(true);
+    menuRef.current.style.transition = "transform 0.5s ease";
+    menuRef.current.style.transform = "translateX(0)";
   };
 
-  const closeCart = () => {
-    cartRef.current.style.transition = "transform 0.5s ease";
-    cartRef.current.style.transform = "translateX(100%)";
-    setIsCartOpen(false);
+  const closeMenu = () => {
+    menuRef.current.style.transition = "transform 0.5s ease";
+    menuRef.current.style.transform = "translateX(-100%)";
+    setIsMenuOpen(false);
   };
 
   const handleTouchStart = (e) => {
     startX.current = e.touches[0].clientX;
     startY.current = e.touches[0].clientY;
-    if (isCartOpen) {
-      cartRef.current.style.transition = "none";
-      cartRef.current.style.willChange = "transform";
+    if (isMenuOpen) {
+      menuRef.current.style.transition = "none";
+      menuRef.current.style.willChange = "transform";
     }
   };
 
@@ -52,8 +50,8 @@ export default function MobileNavBar() {
     const translateY = Math.abs(currentY.current - startY.current);
 
     if (translateX > translateY) {
-      if (isCartOpen) {
-        cartRef.current.style.transform = `translateX(${Math.max(
+      if (isMenuOpen) {
+        menuRef.current.style.transform = `translateX(${Math.min(
           0,
           translateX
         )}px)`;
@@ -64,13 +62,13 @@ export default function MobileNavBar() {
   const handleTouchEnd = () => {
     const swipeDistance = currentX.current - startX.current;
 
-    if (isCartOpen) {
-      cartRef.current.style.willChange = "auto";
-      if (swipeDistance > 100) {
-        closeCart();
+    if (isMenuOpen) {
+      menuRef.current.style.willChange = "auto";
+      if (swipeDistance < -100) {
+        closeMenu();
       } else {
-        cartRef.current.style.transition = "transform 0.2s ease";
-        cartRef.current.style.transform = "translateX(0)";
+        menuRef.current.style.transition = "transform 0.2s ease";
+        menuRef.current.style.transform = "translateX(0)";
       }
     }
   };
@@ -78,39 +76,68 @@ export default function MobileNavBar() {
   return (
     <main>
       <div className="flex justify-between items-center w-full">
+        <div onClick={toggleMenu}>
+          <IoIosMenu className="text-3xl text-amber-500 ml-2" />
+        </div>
         <Link href="/">
           <Image src="/logo.png" width={90} height={90} alt="Logo" />
         </Link>
-
-        <div className="relative" onClick={toggleCart}>
-          <FaCartPlus className="text-2xl text-amber-500 mr-2 cursor-pointer" />
-          <p className="absolute -top-[7px] right-0 bg-red-500 text-white rounded-full w-4 h-4 text-center text-xs">
-            {countCartItems()}
-          </p>
-        </div>
       </div>
 
-      {isCartOpen && (
+      {isMenuOpen && (
         <div
           className={`fixed inset-0 z-40 bg-black transition-opacity duration-500 ${
-            isCartOpen ? "opacity-80" : "opacity-0 pointer-events-none"
+            isMenuOpen ? "opacity-80" : "opacity-0 pointer-events-none"
           }`}
-          onClick={toggleCart}
+          onClick={toggleMenu}
         ></div>
       )}
 
-      {/* Fereastra de coș de la dreapta la stânga */}
+      {/* Meniul de la stânga la dreapta */}
       <div
-        ref={cartRef}
-        className={`fixed inset-0 z-50 transition-transform transform translate-x-full`}
+        ref={menuRef}
+        className={`fixed inset-0 z-50 transition-transform transform -translate-x-full`}
         onTouchStart={handleTouchStart}
         onTouchMove={handleTouchMove}
         onTouchEnd={handleTouchEnd}
       >
-        <div className="absolute right-0 top-0 w-3/4 bg-black h-full shadow-lg shadow-amber-500 overflow-y-auto">
-          <div className="px-4 h-full">
-            <CartProducts toggleCart={toggleCart} isCartOpen={isCartOpen} />
+        <div className="absolute left-0 top-0 w-3/4 bg-black h-full shadow-lg shadow-amber-500 overflow-y-auto">
+          {/* Logo și butonul de închidere */}
+          <div className="lg:hidden flex justify-center items-center p-2 border-b-2 border-amber-500 relative mb-4">
+            <Image src="/logo.png" width={90} height={90} alt="Logo" />
+            <MdClose
+              className="text-3xl cursor-pointer text-white absolute left-4 top-6"
+              onClick={toggleMenu} // Închiderea ferestrei la click pe X
+            />
           </div>
+
+          {/* Link-uri de navigare */}
+          <nav className="flex flex-col gap-8 text-white font-semibold p-4">
+            <Link
+              href="/#Acasa"
+              className="hover:text-amber-500 cursor-pointer"
+            >
+              Acasa
+            </Link>
+            <Link
+              href="/#Produse"
+              className="hover:text-amber-500 cursor-pointer"
+            >
+              Produse
+            </Link>
+            <Link
+              href="/#Despre-noi"
+              className="hover:text-amber-500 cursor-pointer"
+            >
+              Despre Noi
+            </Link>
+            <Link
+              href="/#Recenzii"
+              className="hover:text-amber-500 cursor-pointer"
+            >
+              Recenzii
+            </Link>
+          </nav>
         </div>
       </div>
     </main>
