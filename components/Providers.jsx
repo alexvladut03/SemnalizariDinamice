ccfbe0a7a720ce2d92a60f030249b915b99dcba2
mocanu <mocanuandrei61@gmail.com>
2024-08-30 21:35:04 +0300

    refactor: Remove unused QueryClientProvider in Providers.jsx

--- components/Providers.jsx
+++ components/Providers.jsx
@@ -2,16 +2,11 @@
 
 import React, { useState } from "react";
 import CartProvider from "../utils/context/cart-provider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Providers = ({ children }) => {
   const [queryClient] = useState(() => new QueryClient());
 
-  return (
-    <CartProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </CartProvider>
-  );
+  return <CartProvider>{children}</CartProvider>;
 };
 
 export default Providers;
