9f16a173d7fb389e005c0d161c3536fd80f5bc0d
Ionita Alexandru <alexvladut03@yahoo.com>
2024-09-06 18:41:35 +0300

    refactor: Update Results component to display search value

--- app/(client)/produse/_components/Results.jsx
+++ app/(client)/produse/_components/Results.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function Results() {
+export default function Results({ searchValue }) {
   return (
     <div className="flex ">
       {/* Rezultat cautare */}
-      <div className="text-2xl font-semibold">Produsele noastre</div>
+      <div className="text-2xl font-semibold">
+        Produsele noastre {searchValue}
+        console.log(searchValue);
+      </div>
     </div>
   );
 }
