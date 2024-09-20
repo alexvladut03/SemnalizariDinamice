9482c70f85f285775b4905ed554f22a94c9f69d4
mocanu <mocanuandrei61@gmail.com>
2024-09-20 18:17:55 +0300

    refactor: Update package dependencies and import paths for components

--- app/(admin)/admin/utilizatori/page.js
+++ app/(admin)/admin/utilizatori/page.js
@@ -1,24 +1,38 @@
 import UsersMapping from "./_components/UsersMapping";
 import AddUser from "./_components/AddUser";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getAllUsers } from "@/utils/functions/user/get-all-users";
 
 const page = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["users"],
+    queryFn: getAllUsers,
+  });
+
   return (
     <main className="p-4 bg-gray-100 ">
-      <div className="text-center text-2xl my-8 font-semibold">
-        O lista cu utilizatorii adaugati
-      </div>
-      <div className="flex justify-end mb-4 ">
-        <AddUser />
-      </div>
-
-      <section className="bg-white rounded-lg shadow-sm shadow-gray-400">
-        <div className="grid grid-cols-7 p-2 mb-2 rounded-t-lg font-bold bg-gray-400">
-          <div>Nume</div>
-          <div>Username</div>
-          <div className="col-start-7">Actiuni</div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <div className="text-center text-2xl my-8 font-semibold">
+          O lista cu utilizatorii adaugati
         </div>
-        <UsersMapping />
-      </section>
+        <div className="flex justify-end mb-4 ">
+          <AddUser />
+        </div>
+
+        <section className="bg-white rounded-lg shadow-sm shadow-gray-400">
+          <div className="grid grid-cols-7 p-2 mb-2 rounded-t-lg font-bold bg-gray-400">
+            <div>Nume</div>
+            <div>Username</div>
+            <div className="col-start-7">Actiuni</div>
+          </div>
+          <UsersMapping />
+        </section>
+      </HydrationBoundary>
     </main>
   );
 };
--- app/(admin)/layout.js
+++ app/(admin)/layout.js
@@ -1,12 +1,5 @@
 import { getSession } from "@/utils/get-session";
 import NavbarAdmin from "../../components/layout/admin/admin-navbar";
-import Sidebar from "../../components/layout/admin/admin-sidebar";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-import { getAllUsers } from "@/utils/functions/user/get-all-users";
 import Link from "next/link";
 import { FaHome, FaProductHunt, FaUser } from "react-icons/fa";
 import { MdCategory, MdPermMedia } from "react-icons/md";
@@ -16,74 +9,66 @@ export default async function AdminLayout({ children }) {
 
   if (!session) return <div>Nu esti authentificat.</div>;
 
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["users"],
-    queryFn: getAllUsers,
-  });
-
   return (
     <main>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className="flex bg-gray-100">
-          <div className="flex h-screen bg-gray-100 sticky top-0 ">
-            <div className="w-72 bg-gray-800 text-white flex flex-col p-4 ">
-              <p className="p-4 text-2xl font-bold">Admin Dashboard</p>
-              <Link
-                href="/admin"
-                className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
-              >
-                <FaHome />
-                <div>Dashboard</div>
-              </Link>
-              <Link
-                href="/admin/media"
-                className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
-              >
-                <MdPermMedia />
-                <div>Media</div>
-              </Link>
-              <Link
-                href={"/admin/produse"}
-                className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
-              >
-                <FaProductHunt />
-                <div>Produse</div>
-              </Link>
-              <Link
-                href="/admin/categorii"
-                className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
-              >
-                <MdCategory />
-                <div>Categorii</div>
-              </Link>
-              <Link
-                href="/admin/atribute"
-                className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
-              >
-                <MdCategory />
-                <div>Atribute</div>
-              </Link>
-              <Link
-                prefetch={true}
-                href="/admin/utilizatori"
-                className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
-              >
-                <FaUser />
-                <div>Utilizatori</div>
-              </Link>
-            </div>
-          </div>
-          <div className="flex flex-col w-full">
-            <NavbarAdmin
-              username={session?.user?.name}
-              role={session?.user?.role}
-            />
-
-            {children}
+      <div className="flex bg-gray-100">
+        <div className="flex h-screen bg-gray-100 sticky top-0 ">
+          <div className="w-72 bg-gray-800 text-white flex flex-col p-4 ">
+            <p className="p-4 text-2xl font-bold">Admin Dashboard</p>
+            <Link
+              href="/admin"
+              className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
+            >
+              <FaHome />
+              <div>Dashboard</div>
+            </Link>
+            <Link
+              href="/admin/media"
+              className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
+            >
+              <MdPermMedia />
+              <div>Media</div>
+            </Link>
+            <Link
+              href={"/admin/produse"}
+              className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
+            >
+              <FaProductHunt />
+              <div>Produse</div>
+            </Link>
+            <Link
+              href="/admin/categorii"
+              className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
+            >
+              <MdCategory />
+              <div>Categorii</div>
+            </Link>
+            <Link
+              href="/admin/atribute"
+              className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
+            >
+              <MdCategory />
+              <div>Atribute</div>
+            </Link>
+            <Link
+              prefetch={true}
+              href="/admin/utilizatori"
+              className="flex items-center hover:bg-gray-700 cursor-pointer p-4 gap-2 text-lg"
+            >
+              <FaUser />
+              <div>Utilizatori</div>
+            </Link>
           </div>
         </div>
-      </HydrationBoundary>
+        <div className="flex flex-col w-full">
+          <NavbarAdmin
+            username={session?.user?.name}
+            role={session?.user?.role}
+          />
+
+          {children}
+        </div>
+      </div>
     </main>
   );
 }
