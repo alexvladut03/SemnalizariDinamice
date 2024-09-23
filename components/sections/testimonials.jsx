17c3ff9b306762f75aa69dcf156dac4a4e7c51b6
mocanu <mocanuandrei61@gmail.com>
2024-09-23 13:38:39 +0300

    Revert "refactor: Update testimonials section with improved card components"
    This reverts commit ac61e1c42d3ead4fc84a298adda327b0f85bafef.

--- components/sections/testimonials.jsx
+++ components/sections/testimonials.jsx
@@ -17,18 +17,14 @@ const StarRating = ({ stars }) => (
 // Carduri Masonry diferite
 // LargeCard component
 const LargeCard = ({ videoSrc, name, stars, review, height }) => (
-  <div
-    className={`${height} bg-black p-4 rounded-lg flex w-full transition duration-500 hover:scale-[1.02] shadow-md hover:shadow-amber-500`}
-  >
-    <div className="w-1/2 mr-4">
-      <div className="flex justify-between items-center">
-        <h3 className="text-white text-lg font-bold">{name}</h3>
-        <h3 className="text-white text-sm font-bold">20/09/2024</h3>
-      </div>
+  <div className={`${height} bg-black p-4 rounded-lg shadow-lg flex w-full`}>
+    <div className="w-1/2 p-4">
+      <h3 className="text-white text-lg font-bold">{name}</h3>
       <StarRating stars={stars} />
-      <p className="text-white mt-2">{review}</p>
+      <p className="text-white">{review}</p>
     </div>
     <div className="w-1/2 h-full">
+      {/* Use the Video component here */}
       <Video
         url={videoSrc}
         className="w-full h-full object-cover"
@@ -40,35 +36,26 @@ const LargeCard = ({ videoSrc, name, stars, review, height }) => (
 
 const MediumCard = ({ imageSrc, name, stars, review, height }) => (
   <div
-    className={`${height} bg-black p-4 rounded-lg w-full transition duration-500 hover:scale-[1.02] shadow-md hover:shadow-amber-500`}
+    className={`${height} bg-black p-4 rounded-lg shadow-md shadow-amber-500 w-full `}
   >
     <Image
       src={imageSrc}
       alt={name}
       width={200}
       height={200}
-      className="w-full h-32 object-cover rounded-lg"
+      className="w-full h-32 object-cover mb-4 rounded-lg"
     />
-
-    <div className="flex justify-between items-center mt-2">
-      <h3 className="text-white text-lg font-bold">{name}</h3>
-      <h3 className="text-white text-sm font-bold">20/09/2024</h3>
-    </div>
+    <h3 className="text-white text-lg font-bold">{name}</h3>
     <StarRating stars={stars} />
-    <p className="text-white mt-2">{review}</p>
+    <p className="text-white">{review}</p>
   </div>
 );
 
 const SmallCard = ({ name, stars, review, height }) => (
-  <div
-    className={`${height} bg-black p-4 rounded-lg w-full transition duration-500 hover:scale-[1.02] shadow-md hover:shadow-amber-500`}
-  >
-    <div className="flex justify-between items-center">
-      <h3 className="text-white text-lg font-bold">{name}</h3>
-      <h3 className="text-white text-sm font-bold">20/09/2024</h3>
-    </div>
+  <div className={`${height} bg-black p-4 rounded-lg shadow-lg w-full`}>
+    <h3 className="text-white text-lg font-bold">{name}</h3>
     <StarRating stars={stars} />
-    <p className="text-white mt-2">{review}</p>
+    <p className="text-white">{review}</p>
   </div>
 );
 
@@ -82,7 +69,7 @@ export default function Testimonials() {
       name: "Marian Florin",
       stars: 5,
       review:
-        "Am cumpărat de la acest magazin de două ori niște capace de jante și am fost super multumit de ele. Capacele sunt de calitate si se potrivesc perfect.",
+        "Am cumparat de la acest magazin de 2 ori niste capace de jante si sunt foarte multumit",
       height: "h-72",
     },
 
@@ -90,9 +77,9 @@ export default function Testimonials() {
       id: 1,
       type: "small",
       name: "Octavian Popescu",
-      stars: 4,
+      stars: 5,
       review:
-        "Schimbătorul e destul de ok (ca si calitate) , mi s-a parut cam greu de montat, dar transportul a fost super rapid , mi-a ajuns in 4 zile.",
+        "Semnalizarile dinamice sunt foarte bune , per total sunt multumit",
       height: "h-48",
     },
 
@@ -101,9 +88,9 @@ export default function Testimonials() {
       type: "large",
       videoSrc: "/videos/video-hero.mp4",
       name: "Vlad Dragomir",
-      stars: 5,
+      stars: 4,
       review:
-        "Semnalizările dinamice sunt chiar super mișto, iar instalarea a fost destul de ușoară, am reușit să le montez fără probleme.Livrarea a durat doar 2 zile, ceea ce mi s-a părut rapid, și produsul a venit super bine împachetat, cu siguranta voi recomanda si altor prieteni!",
+        "Schimbarea pe care mi-au adus-o semnalizarile dinamice este uimitoare , sunt super multumit si de trasnport a ajuns foarte repede!",
       height: "h-96",
     },
     // a 2-a coloana
@@ -112,8 +99,7 @@ export default function Testimonials() {
       type: "small",
       name: " Andrei Ionescu",
       stars: 5,
-      review:
-        "Transportul a fost rapid, iar proiectoarele de logo sunt misto , le-am instalat in 5 minute.",
+      review: "Transport rapid si produse de calitate",
       height: "h-36",
     },
 
@@ -123,10 +109,8 @@ export default function Testimonials() {
       type: "medium",
       imageSrc: "/logo.png",
       name: "Mihai Alexandru",
-      stars: 5,
-      review:
-        "Semnalizările dinamice sunt foarte bune.Per total sunt foarte mulțumit de calitatea lor și de transport , recomand!",
-
+      stars: 3,
+      review: "Capacele de jante sunt ok , dar transportul a durat prea mult",
       height: "h-64",
     },
     // a-3 a coloana
@@ -134,9 +118,8 @@ export default function Testimonials() {
       id: 5,
       type: "small",
       name: "Vlad Lina",
-      stars: 4,
-      review:
-        "Semnalizările dinamice arată foarte bine pe mașină.Montajul mi se pare cam greu, dar a reusit sa mi le monteze mecanicul.",
+      stars: 1,
+      review: "Dezamagit de produse , nu recomand",
       height: "h-40",
     },
   ];
@@ -152,10 +135,7 @@ export default function Testimonials() {
 
   useEffect(() => {
     let controls;
-    const finalPosition = -width / 2 - 12;
-
-    console.log("finalPosition", finalPosition);
-    console.log("width", width);
+    const finalPosition = -width / 2 - 10;
 
     if (mustFinish) {
       controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
@@ -190,7 +170,7 @@ export default function Testimonials() {
         </div>
 
         <motion.div
-          className="left-0 flex gap-6 w-[200vw]"
+          className="left-0 flex gap-4 w-[200vw]"
           style={{ x: xTranslation }}
           ref={ref}
           onHoverStart={() => {
