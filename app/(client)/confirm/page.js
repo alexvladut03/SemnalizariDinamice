fe6a8f47e49fbd66085903f500f146a629483bac
mocanu <mocanuandrei61@gmail.com>
2024-11-21 17:21:46 +0200

    testing payment sistem confirmation

--- app/(client)/confirm/page.js
+++ app/(client)/confirm/page.js
@@ -1,7 +1,30 @@
+import prisma from "@/utils/prisma";
+import { notFound } from "next/navigation";
 import React from "react";
 
-const page = () => {
-  return <div>Confirm</div>;
+const page = async ({ searchParams }) => {
+  console.log(searchParams.orderId);
+
+  const order = await prisma.order.findUnique({
+    where: {
+      orderId: searchParams.orderId,
+    },
+  });
+
+  if (!order) {
+    notFound();
+  }
+
+  return (
+    <div>
+      {`Confirm ${order.ntpStatus}`}
+      <div>
+        {order.status.map((status) => (
+          <p key={status}>{status}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- utils/actions/order/update-order-status.js
+++ utils/actions/order/update-order-status.js
@@ -13,7 +13,27 @@ export const updateOrderStatus = async ({ order, payment }) => {
     throw new Error("Order not found");
   }
 
-  console.log(payment.status);
-
-  return order;
+  if (payment.status === 3) {
+    await prisma.order.update({
+      where: {
+        orderId: order.orderID,
+        ntpId: payment.ntpID,
+      },
+      data: {
+        ntpStatus: payment.status,
+        status: ["Pending", "Payment Pending", "Payment Confirmed"],
+      },
+    });
+  } else {
+    await prisma.order.update({
+      where: {
+        orderId: order.orderID,
+        ntpId: payment.ntpID,
+      },
+      data: {
+        ntpStatus: payment.status,
+        status: ["Pending", "Payment Pending", "Payment Failed"],
+      },
+    });
+  }
 };
